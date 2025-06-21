30923d4340b264866d41c6c311d4d1100071c772
Syed-Maaz-Ali-1611 <maazali1611@gmail.com>
2025-06-22 00:14:45 +0500

    Remove Resgister Page From Direct Access

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -119,11 +119,11 @@ const Login = () => {
                         </div>
                         <div className="right md:w-1/2 w-full lg:pl-[60px] md:pl-[40px] flex items-center">
                             <div className="text-content">
-                                <div className="heading4">New Customer</div>
-                                <div className="mt-2 text-secondary">Be part of our growing family of new customers! Join us today and unlock a world of exclusive benefits, offers, and personalized experiences.</div>
-                                <div className="block-button md:mt-7 mt-4">
+                                <div className="heading4">Admin Login</div>
+                                <div className="mt-2 text-secondary">Please login to access admin features and add new products.</div>
+                                {/* <div className="block-button md:mt-7 mt-4">
                                     <Link href={'/register'} className="button-main">Register</Link>
-                                </div>
+                                </div> */}
                             </div>
                         </div>
                     </div>
